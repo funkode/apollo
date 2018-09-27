775cbc17975db1d9d6e8eb1a16b3dcb3d5896a6f
TONG <stong@MTVL16092a0aa.local>
2018-09-27 16:59:11 -0700

    Refacted my election management files.

--- src/components/election-management/ElectionManagement.js
+++ src/components/election-management/ElectionManagement.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import {ToolHeader} from './ToolHeader';
-import {ElectionForm} from './ElectionForm';
+import {CreateElection} from './CreateElection';
+import {withBallotsQuery} from '..queries/ballots/BallotQuery';
+
+const ElectionContainer = withBallotsQuery(CreateElection);
 
 export const ElectionManagement = (props) => {
    return (
    <div>
        <ToolHeader headerText="Election Management" />
-       <ElectionForm voterId={props.voterId}/>
+       <ElectionContainer />
    </div>);
 }
 
