fd426b2f1528830fcb261b4dc46522f801262de0
Tang <Todd_Tang@intuit.com>
2018-09-27 09:36:58 -0700

    clena up

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
 
-import { withToolNameQuery, CarsQuery, CARS_QUERY } from '../queries';
-import { CarFormMutation } from './CarFormMutation';
-import { CarAppendedSubscription } from '../subscriptions/CarAppendedSubscription';
 
-import { CarTable } from './CarTable';
-import { ToolHeader } from './ToolHeader';
 
-const ToolHeaderContainer = withToolNameQuery(({ loading, error, headerText }) => {
-  
-  if (error) {
-    console.log(error);
-  }
-  
-  return <ToolHeader headerText={headerText} />;
-});
 
-const bulkDeleteButtonText = 'Bulk Delete 2';
+
 
 export const App = () =>
   <React.Fragment>
-    <CarAppendedSubscription refetchQueries={[ { query: CARS_QUERY } ]} />
-    <ToolHeaderContainer />
-    <CarsQuery>
-      {props => <CarTable {...props}
-        bulkDeleteButtonText={bulkDeleteButtonText} />}
-    </CarsQuery>
-    <CarFormMutation refetchQueries={[ { query: CARS_QUERY } ]} />
+    <h1>HERE</h1>
   </React.Fragment>;
- 
--- src/components/ItemList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ItemList = props => {
-
-  return <ul>
-    {props.items.map(item => <li key={item}>{item}</li>)}
-  </ul>;
-
-};
