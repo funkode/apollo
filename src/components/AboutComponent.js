7f0914d2cff7f17ee1e7ee547b4f75418680ceea
Pradeep Reddy <preddy1104@gmail.com>
2018-09-28 09:33:51 -0700

    Moved styles to the new file (#33)

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,14 +1,22 @@
-import team from '../images/team.jpg';
 import React from 'react';
+import team from '../images/team.jpg';
+import eric  from '../images/Eric.png';
 export const About = () =>
   <React.Fragment>
     <h3>The glorious team behind this awesome initiative is:</h3>
-    <ul>
-      <li>Dominic Frempong</li>
-      <li>Pradeep Reddy</li>
-      <li>Shelley Tong</li>
-      <li>Todd Tang</li>
-    </ul>
-    <span>Under the guidance of <b>Eric W. Greene</b></span>
-    <img className="teamPic" src={team} alt={"team"}/>
+    <div className="aboutContainer">
+      <div className="leftPanel">
+        <ul>
+          <li>Dominic Frempong</li>
+          <li>Pradeep Reddy</li>
+          <li>Shelley Tong</li>
+          <li>Todd Tang</li>
+        </ul>
+        <img className="teamPic" src={team} alt={"team"}/>
+      </div>
+      <div className="rightPanel">
+        <span class="guideIntro">Under the guidance of <a href="https://git.t4d.io/ericwgreene" target="_blank"><b>Eric W. Greene</b></a></span>
+        <img className="guidePic" src={eric} alt={"Eric W. Greene"}/>
+      </div>
+    </div>
   </React.Fragment>;
