2399abe2a4af7d7f57716d1daa56d4f3c8654c9c
TONG <stong@MTVL16092a0aa.local>
2018-09-28 10:14:35 -0700

    Add election data.

--- src/queries/ballots/BallotQuery.js
+++ src/queries/ballots/BallotQuery.js
@@ -1,26 +1,41 @@
+import React from 'react';
 import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export const BALLOTS_QUERY = gql`
-    query BallotTableQuery {
-        allBallots:getBallots{
-            id
-            name
-            questions {
-                id
-                question
-            }
-            voters
+    query BallotsTableQuery {
+        allBallots:getBallot {
+            id,
+        electionId,
+        userId,
+        votes
         }
     }
 `;
 
 export const withBallotsQuery = graphql(BALLOTS_QUERY, {
-  props : ({ data }) => ({
+  props : ({ data }) => 
+    ({
     loading: data.loading,
     errors: data.error,
     ballots: data.allBallots,
-    //onRefreshCars: () => {},
-    refetchQueries:[ {query: BALLOTS_QUERY } ],
   })
 }) ;
+
+export const BallotsQuery = props =>
+  <Query query={BALLOTS_QUERY}>
+    {({ data, loading, error}) => {
+
+      if (loading) return 'Loading...';
+      
+      if (error) {
+        console.log(error);
+        return null;
+      }
+      console.log("BallotsQuery: ", data);
+
+      return data.allBallots;
+      
+    }}
+  </Query>;
