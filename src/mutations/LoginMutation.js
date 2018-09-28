b181e071e85ec28bd15f052c9ffe6e6615e4a953
Tang <Todd_Tang@intuit.com>
2018-09-28 00:42:42 -0700

    fix login

--- src/mutations/LoginMutation.js
+++ src/mutations/LoginMutation.js
@@ -11,33 +11,52 @@ mutation ($cred:Login){
 
 }
 `;
+const GET_VOTED_IN = gql `
+query($id:ID) {
+  getBallotsVoted(electionId:$id)
+}
+`;
 
 
 
 export const LoginMutation = (props)=>{
-  const finishLogin= (result)=>{
-    console.log("Routing to Votes",result);
 
-    if(result.simpleLogin.authToken){
-      props.history.push('/campaign/'+ props.match.params.ballotId+"/"+result.simpleLogin.id +'/VotePage');
-    }else{
-      //TODO NOTIFICATION
-      console.log("BAD LOGIN");
-    }
+  return <Query query={GET_VOTED_IN} variables={{id:props.match.params.ballotId}}>
+    {({data,loading,error}) => {
+      if (loading) {
+        return null;
+      }
+      if (error) {
+        console.log(error);
+        return null;
+      }
+      const finishLogin= (result)=>{
+        console.log("Routing to Votes",result);
+        console.log("arrayOfBallots",data);
+        console.log("compare",result.simpleLogin.id,data.getBallotsVoted,data.getBallotsVoted.includes(result.simpleLogin.id));
+        if(result.simpleLogin.authToken && !data.getBallotsVoted.includes(result.simpleLogin.id)){
+          props.history.push('/campaign/'+ props.match.params.ballotId+"/"+result.simpleLogin.id +'/VotePage');
+        }else{
+          //TODO NOTIFICATION
+          console.log("BAD LOGIN");
+        }
 
-  };
-  return <Mutation mutation={LOGIN_MUTATION} onCompleted={finishLogin}>
-    {(login)=>{
-      const submitLogin= (credentials)=>{
-        return login(
-          {
-            variables:{cred:credentials}
-          }
-        )
       };
-      console.log("",props.match.params);
-      return<LoginFormComponent {...props} loginForm={submitLogin}/>
-    }
-  }
-  </Mutation>
+      return <Mutation mutation={LOGIN_MUTATION} onCompleted={finishLogin}>
+        {(login)=>{
+          const submitLogin= (credentials)=>{
+            return login(
+              {
+                variables:{cred:credentials}
+              }
+            )
+          };
+          console.log("",props.match.params);
+          return<LoginFormComponent {...props} loginForm={submitLogin}/>
+        }
+      }
+      </Mutation>
+    }}
+
+  </Query>
 }
